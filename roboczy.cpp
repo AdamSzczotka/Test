7296e117045a38e3062e23192ecf46b980f62e5b
Mateusz Wieczorek <mateusz.wieczorek429@gmail.com>
2023-05-23 23:52:55 +0200

    Pole kwadrat

--- roboczy.cpp
+++ roboczy.cpp
@@ -6,7 +6,49 @@ void wyswietla_menu();
 void liczy_kwadrat();
 void liczy_kolo();
 
+class Kwadrat {
+private:
+  float bok;
 
+public:
+  // Konstruktor
+  Kwadrat()
+  {
+
+  }
+
+  // Destruktor
+  ~Kwadrat()
+  {
+
+  }
+
+  // Setter dla boku
+  void ustawBok(float dlugoscBoku) 
+  {
+    while (dlugoscBoku <= 0)
+    {
+        cout<< endl << "Nieporawne dane, bok musi byc wiekszy od 0! Sprobuj jeszcze raz"<< endl;
+        cout<<"Podaj bok: ";
+        cin>>dlugoscBoku;
+    }
+
+    Kwadrat::bok = dlugoscBoku;
+    
+  }
+
+  // Getter dla boku
+  float wyswietlBok() 
+  {
+    return Kwadrat::bok;
+  }
+
+  // Metoda do obliczania pola kwadratu
+  float obliczPole()  
+  {
+    return bok * bok;
+  }
+};
 
 int main(int argc, char const *argv[])
 {
@@ -42,7 +84,7 @@ void wyswietla_menu()
             cout<<"Koniec programu. Autor: Mateusz Wieczorek"<<endl;
             break;
         case '1' :
-
+            liczy_kwadrat();
             break;
         case '2' :
             //wyswietla_pole_prostokat();
@@ -69,5 +111,18 @@ void wyswietla_menu()
         system("pause");
 
     }while (warunek);
+}
+
+void liczy_kwadrat()
+{
+    float bok;
+
+    Kwadrat * kwadrat = new Kwadrat();
+    cout<< endl <<"Podaj bok: ";
+    cin>>bok;
+    kwadrat->ustawBok(bok);
+    cout<<endl<<"Pole kwadratu wynosi: " <<kwadrat->obliczPole() << endl;
+    delete kwadrat;
+
 }
 
